c6c4e5d65f7da00fa84dfd4b4d9ef088b54503bc
Pragatigautam <pragatigautam882@gmail.com>
2022-09-03 04:13:26 +0545

    final

--- script/index.js
+++ script/index.js
@@ -2,37 +2,43 @@ var placeOrderBtn = document.getElementById("placeOrder");
 var stickyElement = document.getElementById("sticky-total");
 var documentWidth = window.innerWidth;
 var elementInView = function (el, dividend) {
-    if (dividend === void 0) { dividend = 1; }
-    var elementTop = el.getBoundingClientRect().top;
-    return (elementTop <=
-        (window.innerHeight || document.documentElement.clientHeight) / dividend);
+  if (dividend === void 0) {
+    dividend = 1;
+  }
+  var elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
 };
 var elementOutofView = function (el) {
-    var elementTop = el.getBoundingClientRect().top;
-    return (elementTop > (window.innerHeight || document.documentElement.clientHeight));
+  var elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
 };
 var displayScrollElement = function (element) {
-    if (documentWidth <= 500) {
-        element.classList.add("show");
-    }
+  if (documentWidth <= 650) {
+    element.classList.add("show");
+  }
 };
 var hideScrollElement = function (element) {
-    element.classList.remove("show");
+  element.classList.remove("show");
 };
 var handleScrollAnimation = function () {
-    if (elementInView(placeOrderBtn, 1.25)) {
-        displayScrollElement(stickyElement);
-    }
-    else if (elementOutofView(placeOrderBtn)) {
-        hideScrollElement(stickyElement);
-    }
+  if (elementInView(placeOrderBtn, 1.25)) {
+    displayScrollElement(stickyElement);
+  } else if (elementOutofView(placeOrderBtn)) {
+    hideScrollElement(stickyElement);
+  }
 };
 window.addEventListener("scroll", function () {
-    handleScrollAnimation();
+  documentWidth = window.innerWidth;
+  handleScrollAnimation();
 });
 window.addEventListener("mousemove", function () {
-    documentWidth = window.innerWidth;
+  documentWidth = window.innerWidth;
 });
 window.addEventListener("load", function () {
-    handleScrollAnimation();
+  handleScrollAnimation();
 });
