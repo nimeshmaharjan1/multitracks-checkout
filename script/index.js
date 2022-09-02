8184507234af8f6ef909e2a31f3e66ee960e9288
Nimesh Maharjan <nimesh.maharjan>
2022-09-02 17:07:57 +0545

    mobile desigin completed

--- script/index.js
+++ script/index.js
@@ -1,1 +1,38 @@
-console.log("hello");
+var placeOrderBtn = document.getElementById("placeOrder");
+var stickyElement = document.getElementById("sticky-total");
+var documentWidth = window.innerWidth;
+var elementInView = function (el, dividend) {
+    if (dividend === void 0) { dividend = 1; }
+    var elementTop = el.getBoundingClientRect().top;
+    return (elementTop <=
+        (window.innerHeight || document.documentElement.clientHeight) / dividend);
+};
+var elementOutofView = function (el) {
+    var elementTop = el.getBoundingClientRect().top;
+    return (elementTop > (window.innerHeight || document.documentElement.clientHeight));
+};
+var displayScrollElement = function (element) {
+    if (documentWidth <= 500) {
+        element.classList.add("show");
+    }
+};
+var hideScrollElement = function (element) {
+    element.classList.remove("show");
+};
+var handleScrollAnimation = function () {
+    if (elementInView(placeOrderBtn, 1.25)) {
+        displayScrollElement(stickyElement);
+    }
+    else if (elementOutofView(placeOrderBtn)) {
+        hideScrollElement(stickyElement);
+    }
+};
+window.addEventListener("scroll", function () {
+    handleScrollAnimation();
+});
+window.addEventListener("mousemove", function () {
+    documentWidth = window.innerWidth;
+});
+window.addEventListener("load", function () {
+    handleScrollAnimation();
+});
--- script/index.ts
+++ script/index.ts
@@ -1,1 +1,47 @@
-console.log("hello");
+const placeOrderBtn = document.getElementById("placeOrder") as HTMLElement;
+const stickyElement = document.getElementById("sticky-total") as HTMLElement;
+let documentWidth = window.innerWidth;
+const elementInView = (el: HTMLElement, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el: HTMLElement) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element: HTMLElement) => {
+  if (documentWidth <= 500) {
+    element.classList.add("show");
+  }
+};
+
+const hideScrollElement = (element: HTMLElement) => {
+  element.classList.remove("show");
+};
+
+const handleScrollAnimation = () => {
+  if (elementInView(placeOrderBtn, 1.25)) {
+    displayScrollElement(stickyElement);
+  } else if (elementOutofView(placeOrderBtn)) {
+    hideScrollElement(stickyElement);
+  }
+};
+
+window.addEventListener("scroll", () => {
+  handleScrollAnimation();
+});
+window.addEventListener("mousemove", () => {
+  documentWidth = window.innerWidth;
+});
+window.addEventListener("load", () => {
+  handleScrollAnimation();
+});
